7cf6308498ff0c50956e422b5a500cc33692826b
Hacktivator <118433308+Hackt1vator@users.noreply.github.com>
2023-05-02 17:13:53 +0200

    Add files via upload

--- applera1n.py
+++ applera1n.py
@@ -37,6 +37,21 @@ def showDFUMessage():
 
 
 
+def clear():
+
+
+
+    folders_to_delete = ['./palera1n/blobs', './palera1n/work']
+    for folder in os.listdir('./palera1n'):
+        if folder.startswith('boot'):
+            folders_to_delete.append(f'./palera1n/{folder}')
+
+    for folder in folders_to_delete:
+        if os.path.exists(folder):
+            os.system(f'rm -rf {folder}')
+            print(f'{folder} deleted.')
+
+    messagebox.showinfo("Done","applera1n files cleared")
 
 
 def quitProgram():
@@ -47,24 +62,87 @@ def opentwitter():
     webbrowser.open('https://www.twitter.com/hackt1vator', new=2)
     
 def startbypass():
-
     global LAST_CONNECTED_UDID, LAST_CONNECTED_IOS_VER
-    
-    #iOSVER = str(LAST_CONNECTED_IOS_VER)
-    iOSVer = askstring('Device iOS?', 'On what ios version are you?')
-    
-    #check if theres a valid string to continue to reversing jb
-    if(len(iOSVer) < 2):
-        showinfo('Bypass Failed', 'Give me a valid iOS version.')
-    else:
-        showinfo('Ready to Jailbreak...', 'Hi, iOS '+str(iOSVer)+'. \n\nWe will now bypass your device '+str(iOSVer)+' Semi-Tethered.')
-        print("Starting bypass...")
-        os.system(f"cd ./palera1n/ && ./palera1n.sh --tweaks {iOSVer} --semi-tethered")
-        
-        print("Device is bypassed!\n")
-        showinfo('Bypass Success!', 'Device is now bypassed!')
 
-    
+#iOSVER = str(LAST_CONNECTED_IOS_VER)
+    global LAST_CONNECTED_UDID, LAST_CONNECTED_IOS_VER
+    # step 1 technically
+    print("Searching for connected device...")
+    os.system("idevicepair unpair")
+    os.system("idevicepair pair")
+    os.system("./device/ideviceinfo > ./device/lastdevice.txt")
+
+    time.sleep(2)
+
+    f = open("./device/lastdevice.txt", "r")
+    fileData = f.read()
+    f.close()
+
+    if ("ERROR:" in fileData):
+        # no device was detected, so retry user!
+        print("ERROR: No device found!")
+
+        messagebox.showinfo("No device detected! 0x404", "Try disconnecting and reconnecting your device.")
+    else:
+        # we definitely have something connected...
+
+        # find the UDID
+        start = 'UniqueDeviceID: '
+        end = 'UseRaptorCerts:'
+        s = str(fileData)
+
+        foundData = s[s.find(start) + len(start):s.rfind(end)]
+        UDID = str(foundData)
+        LAST_CONNECTED_UDID = str(UDID)
+
+        # find the iOS
+        # we definitely have something connected...
+        start2 = 'ProductVersion: '
+        end2 = 'ProductionSOC:'
+        s2 = str(fileData)
+
+        foundData2 = s2[s.find(start2) + len(start2):s2.rfind(end2)]
+        deviceIOS = str(foundData2)
+        LAST_CONNECTED_IOS_VER = str(deviceIOS)
+
+        if (len(UDID) > 38):
+            # stop automatic detection
+            timerStatus = 0
+
+            print("Found UDID: " + LAST_CONNECTED_UDID)
+            messagebox.showinfo("iDevice is detected!", "Found iDevice on iOS " + LAST_CONNECTED_IOS_VER)
+        #            cbeginExploit10["state"] = "normal"
+        #            cbeginExploit2["state"] = "normal"
+
+        # messagebox.showinfo("Ready to begin!","We are ready to start jailbreak!")
+
+        # cbeginExploit10["state"] = "normal"
+
+        else:
+            print("Couldn't find your device")
+            messagebox.showinfo("Somethings missing! 0x405", "Try disconnecting and reconnecting your device.")
+
+    #check if theres a valid string to continue to reversing jb
+    if(len(LAST_CONNECTED_IOS_VER) < 2):
+        showinfo('jailbreak Failed', 'Give me a valid iOS version.')
+    else:
+        showinfo('Ready to Jailbreak...', 'Hi, iOS '+str(LAST_CONNECTED_IOS_VER)+'. \n\nWe will now bypass iOS '+str(LAST_CONNECTED_IOS_VER)+' Semi-Tethered.')
+        print("Starting jailbreak...")
+        os.system("idevicepair unpair")
+        os.system("idevicepair pair")
+        os.system(f"cd ./palera1n/ && ./palera1n.sh --tweaks --semi-tethered {LAST_CONNECTED_IOS_VER}")
+
+        print("Device is bypassed!\n")
+        showinfo('bypass Success!', 'Device is now bypassed!')
+
+
+def enterRecMode():
+    print("Kicking device into recovery mode...")
+    os.system("./device/enterrecovery.sh")
+
+def exitRecMode():
+    print("Kicking device out recovery mode...")
+    os.system("./device/exitrecovery.sh")
 
     
 
@@ -91,7 +169,7 @@ my_label2.place(x=130, y=130)
 
 #label
 my_label3 = Label(frame,
-                 text = "ver 1.4")
+                 text = "ver 1.5")
 my_label3.place(x=10, y=220)
 
 
@@ -101,6 +179,21 @@ cButton1 = tk.Button(frame,
                    command=startbypass,
                    state="normal")
 cButton1.place(x=180, y=160)
+cButton2 = tk.Button(frame,
+                   text="clear",
+                   command=clear,
+                   state="normal")
+cButton2.place(x=295, y=160)
+cButton3 = tk.Button(frame,
+                   text="enter recovery",
+                   command=enterRecMode,
+                   state="normal")
+cButton3.place(x=10, y=10)
+cButton4 = tk.Button(frame,
+                 text="exit recovery",
+                 command=exitRecMode,
+                 state="normal")
+cButton4.place(x=380, y=10)
 #Create a Label to display the link
 link = Label(root, text="Made this tool @hackt1vator",font=('Helveticabold', 12), cursor="hand2")
 link.place(x=165, y=220)
